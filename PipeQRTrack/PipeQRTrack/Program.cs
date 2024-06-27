8f3fb5c5a6176faec867b61f0fc0eef8aa77da22
Woosung Nam <nws400@gmail.com>
2024-06-27 16:01:29 -0500

    fix: build error fix

--- PipeQRTrack/PipeQRTrack/Program.cs
+++ PipeQRTrack/PipeQRTrack/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using PipeQRTrack.Client.Pages;
 using PipeQRTrack.Components;
 using PipeQRTrack.Components.Account;
 using PipeQRTrack.Data;
