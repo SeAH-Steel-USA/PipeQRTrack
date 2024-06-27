d4ad89355b776b2b1222caa636f45cb14f46a6a3
Woosung Nam <nws400@gmail.com>
2024-06-27 16:08:53 -0500

    chore: add migration

--- PipeQRTrack/PipeQRTrack/Data/Migrations/20240627210322_initsetup.Designer.cs
+++ PipeQRTrack/PipeQRTrack/Data/Migrations/20240627210322_initsetup.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PipeQRTrack.Data;
 
@@ -11,9 +12,11 @@ using PipeQRTrack.Data;
 namespace PipeQRTrack.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240627210322_initsetup")]
+    partial class initsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PipeQRTrack/PipeQRTrack/Data/Migrations/20240627210322_initsetup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PipeQRTrack.Migrations
+{
+    /// <inheritdoc />
+    public partial class initsetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
