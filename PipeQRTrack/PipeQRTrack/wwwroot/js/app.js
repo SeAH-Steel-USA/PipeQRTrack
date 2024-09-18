d4a98dc25c125867bf0042411488e1e87d844929
Woosung Nam <nws400@gmail.com>
2024-09-18 14:50:01 -0500

    feat: print function

---
+++ PipeQRTrack/PipeQRTrack/wwwroot/js/app.js
@@ -0,0 +1,39 @@
+﻿window.printImage = function (imageUrl) {
+    var win = window.open('');
+    win.document.write(`
+        <html>
+        <head>
+            <style>
+                body {
+                    margin: 0;
+                    padding: 0;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                }
+                img {
+                    max-width: 100%;
+                    max-height: 100%;
+                    object-fit: contain;
+                }
+                @media print {
+                    @page {
+                        size: auto;
+                        margin: 0mm;
+                    }
+                    body {
+                        margin: 0;
+                        padding: 0;
+                    }
+                }
+            </style>
+        </head>
+        <body>
+            <img src="${imageUrl}" onload="window.print();window.close()"/>
+        </body>
+        </html>
+    `);
+    win.document.close();
+    win.focus();
+}
