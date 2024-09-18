666b2ad666ab8a90baa8d420df99075f9c28a8ec
Woosung Nam <nws400@gmail.com>
2024-09-18 17:33:08 -0500

    feat: possibly working zxing

--- PipeQRTrack/PipeQRTrack/Program.cs
+++ PipeQRTrack/PipeQRTrack/Program.cs
@@ -10,6 +10,8 @@ using PipeQRTrack.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
